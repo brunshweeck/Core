6516888095bdf3a0184015a6736b0138787a0017
brunshweeck <brunshweeck1234@gmail.com>
2023-03-21 12:10:58 +0100

    test8

---
+++ Collections/HashMap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brunshweeck on 20/03/2023.
+//
+
+#include "HashMap.h"
---
+++ Collections/HashMap.h
@@ -0,0 +1,14 @@
+//
+// Created by brunshweeck on 20/03/2023.
+//
+
+#ifndef CORE_HASHMAP_H
+#define CORE_HASHMAP_H
+
+
+class HashMap {
+
+};
+
+
+#endif //CORE_HASHMAP_H
